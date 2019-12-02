6f78abdd8690e23a6caacd7a740e3dada40746e0
harrypatrick442 <34253839+harrypatrick442@users.noreply.github.com>
2019-12-02 20:24:04 +0000

    full compilation now working

---
+++ index.js
@@ -0,0 +1,6 @@
+const RepositoriesHelper= require('repositories_helper');module.exports={
+	getScriptsRelativePath:RepositoriesHelper.getGetScriptsRelativePath(),
+	getScriptsAbsolutePath:RepositoriesHelper.getGetScriptsAbsolutePath(),
+	getStylesRelativePath:RepositoriesHelper.getGetStylesRelativePath(),
+	getStylesAbsolutePath:RepositoriesHelper.getGetStylesAbsolutePath()
+};
