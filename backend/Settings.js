31e4f049a86712e21a2f10d2ff0330972705a0b5
harrypatrick442 <34253839+harrypatrick442@users.noreply.github.com>
2020-01-10 07:34:16 +0000

    about to put in place database

--- backend/Settings.js
+++ backend/Settings.js
@@ -1,17 +1,23 @@
-module.exports = (function(params){
+module.exports = (function(){
 	var _settings;
 	function Settings(){
+		this.getSharsUser = function(){
+			return params.shardsUser;
+		};
 		this.getHostIdShardCreator = function(){
 			return params.hostIdShardCreator;
-		};
+		}; 
 	}
 	this.get = function(){
 		return new Promise((resolve, reject)=>{
 			if(_settings){resolve(_settings);return;}
-			DalPms.getSettings().then((settings)=>{
+			require('./DalPms').getSettings().then((settings)=>{
 				_settings = settings;
 				resolve(settings);
 			}).catch(reject);
 		});
 	});
+	this.fromSqlRow = function(row){
+		return new Settings(row);
+	};
 })();
