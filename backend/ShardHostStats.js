160a45e9aed5b006f94e41212722f8f395906022
harrypatrick442 <34253839+harrypatrick442@users.noreply.github.com>
2020-01-12 13:31:32 +0000

    shard host stats

---
+++ backend/ShardHostStats.js
@@ -0,0 +1,12 @@
+module.exports = function(params){
+	this.getNUsers = function(){
+		return params.nUsers;
+	};
+	this.getLoadHandlingFactor = function(){
+		return getShardHost().getLoadHandlingFactor();
+	};
+	this.getShardHost = getShardHost;
+	function getShardHost(){
+		return params.shardHost;
+	}
+};
