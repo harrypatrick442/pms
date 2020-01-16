19305586948e9c5b963788a4da082b2afa1c894f
harrypatrick442 <34253839+harrypatrick442@users.noreply.github.com>
2020-01-16 08:52:48 +0000

    debugging

--- backend/PmsShardBuilder.js
+++ backend/PmsShardBuilder.js
@@ -41,7 +41,7 @@ module.exports = new (function(params){
 			new TableColumn({name:'userIdFrom', type:TableColumnTypes.INT, nullable:false}),
 			new TableColumn({name:'userIdTo', type:TableColumnTypes.INT, nullable:false}),
 			new TableColumn({name:'sentAt', type:TableColumnTypes.DATETIME, nullable:false}),
-			new TableColumn({name:'content', type:TableColumnTypes.TEXT, nullable:false})
+			new TableColumn({name:'content', type:TableColumnTypes.VARCHAR, length:640, nullable:false})
 		]
 	});
 	const tableTypes =[tableTypePms];
