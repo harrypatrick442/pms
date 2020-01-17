c9088ddfd91a953e168d2c4437e22803864cfe54
harrypatrick442 <34253839+harrypatrick442@users.noreply.github.com>
2020-01-17 14:18:38 +0000

    creating deleteShards for practical purposes

--- backend/DalPms.js
+++ backend/DalPms.js
@@ -2,14 +2,15 @@ module.exports = new(function(){
 	const STORED_PROCEDURE_SHARDS_GET= 'pms_shards_get',
 	STORED_PROCEDURE_SETTINGS_GET='pms_settings_get',
 	STORED_PROCEDURE_SHARD_ADD='pms_shard_add',
-	STORED_PROCEDURE_SHARD_HOSTS_GET='pms_shard_hosts_get';
+	STORED_PROCEDURE_SHARD_HOSTS_GET='pms_shard_hosts_get',
+	STORED_PROCEDURE_SHARDS_DELETE='pms_shards_delete';
     const Dal = require('dal');	
 	const sql = Dal.sql;
 	const Core = require('core');
 	const S = require('strings').S;
 	const Iterator = Core.Iterator;
 	var dal;
-	const HOST_ID='hostId', NAME='name', USER_ID_FROM_INCLUSIVE='userIdFromInclusive', USER_ID_TO_EXCLUSIVE='userIdToExclusive';
+	const HOST_ID='hostId', NAME='name', USER_ID_FROM_INCLUSIVE='userIdFromInclusive', USER_ID_TO_EXCLUSIVE='userIdToExclusive', IDS='ids';
 	this.initialize = function(configuration){
 		dal = new Dal(configuration);
 	};
@@ -62,6 +63,14 @@ module.exports = new(function(){
 				}
 			}).catch(reject);
 		});
+	};
+	this.deleteShards = function(shardIds){
+		return dal.query({
+				storedProcedure:STORED_PROCEDURE_SHARDS_DELETE,
+				parameters:[
+					{name:IDS, value:shardIds,type:sql.Varchar(2000)}
+				]
+			});
 	};
 	this.getShardHosts = function(){
 		return new Promise(function(resolve, reject){
